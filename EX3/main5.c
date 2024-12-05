29af0c8fd098daa2950349d1b7f15c240bb8ea59
SkyLockatf1 <43670915+SkyLockatf1@users.noreply.github.com>
2024-12-05 14:27:32 +0800

    change EX3@main5.c

--- EX3/main5.c
+++ EX3/main5.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    int i = 151;
+    int i = 241;
     int total = 0;
     if(i < 30){
         printf("免費");
-    }else if(i >= 480){
+    }else if(i >= 240){
         printf("240元");        
     }else{
         if(i%30){
