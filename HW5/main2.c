395b31931828de548c5a232b973bae4cacff95f1
SkyLockatf1 <43670915+SkyLockatf1@users.noreply.github.com>
2024-12-10 19:26:54 +0800

    change HW5@main2.c

--- HW5/main2.c
+++ HW5/main2.c
@@ -3,7 +3,7 @@
 int main(){
     double PI = 0;
     int sign = 1; // 1=+ -1=-
-    int x = 261315;
+    int x = 300000;
     for(int i=1;i<=x;i+=2){
         if(sign==1){
             PI +=4.0/i*sign;
@@ -12,12 +12,10 @@ int main(){
             PI += 4.0/i*sign;
             sign = 1;
         }
-        // printf("i:%d || %.5f\n",i,PI);
         if((int)(PI*100000+0.5)/100000.0 ==3.14159){
-            printf("i:%d || %.5f\n",i,PI);
+            printf("x:%d Value:%.5f (5 places of decimals)\n",i,PI);
             break;
         }
     }
-    printf("%.5f\n",PI);
     return 0;
 }
